42ce9d9f1a3ac948f9fcd1b3b3e926b8a4da9099
hassanakram-n <hassanakram.n@gmail.com>
2022-05-01 00:36:43 +0500

    replaced the skills with mine

--- src/components/Technologies/Skills.js
+++ src/components/Technologies/Skills.js
@@ -1,42 +1,82 @@
-import { DiAndroid, DiTerminal } from "react-icons/di";
-import { SiJava, SiFirebase, SiGit } from "react-icons/si";
-import { RiFlutterFill } from "react-icons/ri";
+import { DiReact, DiJsBadge, DiDatabase, DiJavascript1 } from "react-icons/di";
+import { SiTypescript, SiFirebase, SiGit } from "react-icons/si";
 
 export const Skills = [
   {
-    slug: "android",
-    Component: DiAndroid,
-    title: "Android",
-    Description: () => <>I work on ROMs, Kernels & Android Apps using Java.</>,
+    slug: "javascript",
+    Component: DiJsBadge,
+    title: "Javascript",
+    Description: () => (
+      <>
+        I am a Javascript Developer, javascript is my first programing language.
+      </>
+    ),
   },
   {
-    slug: "flutter",
-    Component: RiFlutterFill,
-    title: "Flutter",
-    Description: () => <>Currently working with Dart & Flutter for app development. </>,
+    slug: "typescript",
+    Component: SiTypescript,
+    title: "Typescript",
+    Description: () => (
+      <>
+        I have use typescript in multiple projects, as it reduces run time
+        errors .
+      </>
+    ),
   },
   {
-    slug: "java",
-    Component: SiJava,
-    title: "Java",
-    Description: () => <>I can code in Java. But I mainly use it for Android app development.</>,
+    slug: "reactJs",
+    Component: DiReact,
+    title: "ReactJs",
+    Description: () => (
+      <>I can Develop Dynamic Web Applications using React Js</>
+    ),
   },
   {
-    slug: "terminal",
-    Component: DiTerminal,
-    title: "Scripting",
-    Description: () => <>I have written dozens of Shell scripts for various purposes.</>,
+    slug: "nextJs",
+    Component: DiJavascript1,
+    title: "NextJs",
+    Description: () => (
+      <>
+        I have used nextJs in simple & also large websites, I can make api's in
+        nextJs.
+      </>
+    ),
+  },
+  {
+    slug: "reactNative",
+    Component: DiReact,
+    title: "React Native",
+    Description: () => (
+      <>I have developed Android & IOS Native Apps using React Native.</>
+    ),
+  },
+  {
+    slug: "redux",
+    Component: DiDatabase,
+    title: "Redux",
+    Description: () => (
+      <>
+        I prefer to use redux to manage application state, & redux-saga as a
+        middleware.
+      </>
+    ),
   },
   {
     slug: "firebase",
     Component: SiFirebase,
     title: "Firebase",
-    Description: () => <>I have used Firebase for auth, database & analytics in my apps.</>,
+    Description: () => (
+      <>
+        I have used Firebase for auth, database, hosting & analytics in my apps.
+      </>
+    ),
   },
   {
     slug: "git",
     Component: SiGit,
     title: "Git",
-    Description: () => <>Git is a tool that I use every day. I use GitHub for pushing my code.</>,
+    Description: () => (
+      <>Git is a tool that I use every day. I use GitHub for pushing my code.</>
+    ),
   },
 ];
