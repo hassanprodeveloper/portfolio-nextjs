d445823c4ee361f5a32aa1f605f1c3ea435481ee
hassanakram-n <hassanakram.n@gmail.com>
2022-05-01 00:35:24 +0500

    changed title of application

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,7 +5,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Vipul Jha - Android & Flutter Dev</title>
+        <title>Hassan Akram - React Developer</title>
       </Head>
       <Theme>
         <Component {...pageProps} />
