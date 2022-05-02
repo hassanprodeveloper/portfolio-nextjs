44aa6b4db5876af0f95f934d03f8527775f59640
hassanakram-n <hassanakram.n@gmail.com>
2022-05-03 01:40:32 +0500

    firestore/contact file code refactored

--- src/firebase/firestore/contact.js
+++ src/firebase/firestore/contact.js
@@ -4,13 +4,12 @@ import { contactDb } from ".";
 
 export const submitContactMessage = async (data = {}) => {
   const createdAt = new Date().getTime();
+
   try {
-    console.log("submitContactMessage data", data);
-    const response = await addDoc(contactDb, { ...data, createdAt });
-    console.log("submitContactMessage response", response);
+    await addDoc(contactDb, { ...data, createdAt });
+
     return !!1;
   } catch (error) {
-    console.log("submitContactMessage error", error);
     return !!0;
   }
 };
