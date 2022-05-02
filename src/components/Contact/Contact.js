1ebc031d3c08199eaffbbe38fdaf82ff04c6e16e
hassanakram-n <hassanakram.n@gmail.com>
2022-05-03 01:38:28 +0500

    get in touch form data is submitted to firestore completed

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,40 +1,137 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 
 import {
+  ButtonBack,
+  ButtonFront,
   Section,
   SectionDivider,
   SectionTitle,
   Title,
 } from "../../styles/GlobalComponents";
-import Button from "../../styles/GlobalComponents/Button";
+
+import { submitContactMessage } from "../../firebase/firestore/contact";
 
 import { WrapperGrid, Input, Textarea, Form } from "./ContactStyles";
 
-const Contact = () => (
-  <Section>
-    <SectionTitle>Get In Touch </SectionTitle>
-    <Form>
-      <WrapperGrid>
-        <Title>Name</Title>
-        <Input type="text" name="name" />
-      </WrapperGrid>
+const defaultMessage = {
+  name: "",
+  email: "",
+  message: "",
+};
 
-      <WrapperGrid>
-        <Title>Email</Title>
-        <Input type="email" name="email" />
-      </WrapperGrid>
+const Contact = () => {
+  const [submitting, setSubmitting] = useState(false);
+  const [submittedMessage, setSubmittedMessage] = useState(null);
+  const [message, setMessage] = useState(defaultMessage);
 
-      <WrapperGrid full>
-        <Title>Message</Title>
-        <Textarea name="message" rows="5"></Textarea>
-      </WrapperGrid>
+  const handleSetMessage = (element) => {
+    let key = element.target.name;
+    let value = element.target.value;
 
-      <WrapperGrid full>
-        <Button>Submit</Button>
-      </WrapperGrid>
-    </Form>
-    <SectionDivider />
-  </Section>
-);
+    setMessage((prev) => ({ ...prev, [key]: value }));
+  };
+
+  const handleSubmit = async () => {
+    if (message.name && message.email && message.message)
+      try {
+        setSubmitting(true);
+        const submitted = await submitContactMessage(message);
+
+        if (submitted) {
+          setMessage(defaultMessage);
+
+          setSubmittedMessage({
+            type: "success",
+            text: "Thanks for getting in touch, I will get back to you soon.",
+          });
+        } else {
+          setSubmittedMessage({
+            type: "error",
+            text: "Something went wrong, check your internet and try again.",
+          });
+        }
+
+        setSubmitting(false);
+      } catch (error) {
+        setSubmittedMessage({
+          type: "error",
+          text: "Something went wrong, check your internet and try again.",
+        });
+
+        setSubmitting(false);
+      }
+  };
+
+  useMemo(() => {
+    if (!!submittedMessage) {
+      setTimeout(() => {
+        setSubmittedMessage(null);
+      }, 2000);
+    }
+  }, [submittedMessage]);
+
+  return (
+    <Section>
+      <SectionTitle>Get In Touch </SectionTitle>
+      <Form>
+        <WrapperGrid>
+          <Title>Name</Title>
+          <Input
+            type="text"
+            disabled={submitting}
+            name="name"
+            value={message.name}
+            onChange={handleSetMessage}
+          />
+        </WrapperGrid>
+
+        <WrapperGrid>
+          <Title>Email</Title>
+          <Input
+            type="email"
+            disabled={submitting}
+            name="email"
+            value={message.email}
+            onChange={handleSetMessage}
+          />
+        </WrapperGrid>
+
+        <WrapperGrid full>
+          <Title>Message</Title>
+          <Textarea
+            disabled={submitting}
+            name="message"
+            rows="5"
+            value={message.message}
+            onChange={handleSetMessage}
+          />
+        </WrapperGrid>
+
+        <WrapperGrid full>
+          {!!submittedMessage && (
+            <span
+              style={{
+                color: submittedMessage.type === "success" ? "green" : "red",
+              }}
+            >
+              {submittedMessage.text}
+            </span>
+          )}
+          <ButtonBack disabled={submitting}>
+            {submitting ? "Loading..." : "Submit"}
+            <ButtonFront
+              type="button"
+              disabled={submitting}
+              onClick={handleSubmit}
+            >
+              {submitting ? "Loading..." : "Submit"}
+            </ButtonFront>
+          </ButtonBack>
+        </WrapperGrid>
+      </Form>
+      <SectionDivider />
+    </Section>
+  );
+};
 
 export default Contact;
