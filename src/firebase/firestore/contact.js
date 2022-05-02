7df6345d1b6393cbb7167c4addc0c0f42d0db67e
hassanakram-n <hassanakram.n@gmail.com>
2022-05-03 01:02:19 +0500

    renamed getIntouch file to contact

--- src/firebase/firestore/contact.js
+++ src/firebase/firestore/contact.js
@@ -3,8 +3,10 @@ import { addDoc } from "firebase/firestore";
 import { contactDb } from ".";
 
 export const submitContactMessage = async (data = {}) => {
+  const createdAt = new Date().getTime();
   try {
-    const response = await addDoc(contactDb, data);
+    console.log("submitContactMessage data", data);
+    const response = await addDoc(contactDb, { ...data, createdAt });
     console.log("submitContactMessage response", response);
     return !!1;
   } catch (error) {
