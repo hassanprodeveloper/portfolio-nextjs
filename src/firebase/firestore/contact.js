afada135ca850f351f01f4db41e281dd82c329a8
hassanakram-n <hassanakram.n@gmail.com>
2022-05-05 13:27:51 +0500

    greating email sending functionality added

--- src/firebase/firestore/contact.js
+++ src/firebase/firestore/contact.js
@@ -2,12 +2,20 @@ import { addDoc } from "firebase/firestore";
 
 import { contactDb } from ".";
 
+import { apiPost } from "../../utils";
+
 export const submitContactMessage = async (data = {}) => {
   const createdAt = new Date().getTime();
 
   try {
     await addDoc(contactDb, { ...data, createdAt });
-
+    await apiPost(`/api/email`, { email: data.email })
+      .then((res) => {
+        console.log("email confirmation sent",data.email);
+      })
+      .catch((err) => {
+        console.log("email sending error ", err.message);
+      });
     return !!1;
   } catch (error) {
     return !!0;
