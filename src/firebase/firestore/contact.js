94b81c7cbcd1a62170cef36aeaa157931c1b579d
hassanakram-n <hassanakram.n@gmail.com>
2022-05-05 23:38:13 +0500

    contact firestore function updated

--- src/firebase/firestore/contact.js
+++ src/firebase/firestore/contact.js
@@ -7,10 +7,10 @@ import { apiPost } from "../../utils";
 export const submitContactMessage = async (data = {}) => {
   const createdAt = new Date().getTime();
 
-  try {
-    const apiResp = await apiPost(`/api/email`, { email: data.email });
+  const apiResp = await apiPost(`/api/email`, { email: data.email });
 
-    await addDoc(contactDb, { ...data, createdAt, emailSent: !!apiResp.ok });
+  try {
+    await addDoc(contactDb, { ...data, createdAt, emailSent: !!apiResp?.ok });
 
     return !!1;
   } catch (error) {
