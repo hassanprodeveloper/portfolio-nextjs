edf097c4477990e2c804f86e7deaab4ea26d9b23
hassanakram-n <hassanakram.n@gmail.com>
2023-03-15 01:17:03 +0500

    change theme

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,11 @@
-export const CONTACT_FIRESTORE_COL = "contactMessages";
+export const FULL_NAME = "Muhammad Hassan"
+export const FIRST_NAME = "Muhammad"
+export const LAST_NAME = "Hassan"
+export const PROFILE_IMAGE_URL = 'https://avatars.githubusercontent.com/u/72764487?s=400&u=1f60b836c44aa3818a5f1d7a048b70069def9054'
+
+export const EMAIL_ADDRESS = "hassanakram.n@gmail.com"
+export const PHONE_NUMBER = "92 304 7955183"
+export const PERSONAL_ADDRESS = "Lahore"
+export const NATIONALITY = "Pakistan"
+export const LANGUAGE = ["English", "Urdu"]
+export const AGE = 23
