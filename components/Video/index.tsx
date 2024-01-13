90001d1a9c6ef779a1bb37000569ea96a6ce6603
hassanprodeveloper <hassanprodeveloper@gmail.com>
2024-01-14 01:28:32 +0500

    add youtube video id

--- components/Video/index.tsx
+++ components/Video/index.tsx
@@ -54,7 +54,7 @@ const Video = () => {
         autoplay={true}
         start={true}
         isOpen={isOpen}
-        videoId="L61p2uyiMSo"
+        videoId="sOjj-K9fYds"
         onClose={() => setOpen(false)}
       />
 
